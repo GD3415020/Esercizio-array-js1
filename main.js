ec3d99b85f5216dee328f5e213a121c5f9e088bb
GDIsanto <logantom85@gmail.com>
2025-11-25 16:45:54 +0100

    esercizio completato

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('ciao');
+let arrayCasuale = [4, 54, 554, 3, -23, -50, 7, 78, 66, -400];
+
+console.log(arrayCasuale);
+
+
+
+// ordine decrescente
+
+let arrayDecresc = arrayCasuale.sort(function (a, b) {
+    return b - a;
+});
+
+console.log(arrayDecresc);
+
+
+
+// ordine crescente
+
+let arrayCresc = arrayCasuale.sort(function (a, b) {
+    return a - b;
+});
+
+console.log(arrayCresc);
+
